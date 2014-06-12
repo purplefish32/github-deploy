5091637d97a00900ddaff9c229508809f97fb46f
Donovan Tengblad <purplefish32@gmail.com>
2014-06-12 14:51:16 +0200

    Added basic app that logs github payload

---
+++ app/bootstrap/app.php
@@ -0,0 +1,10 @@
+<?php
+
+$app->post('/', function () use ($app, $config) {
+
+    $payload = $app->request()->getBody()
+
+    $json = json_decode($payload);
+
+    $app->log->info("payload : " . $json);
+});
