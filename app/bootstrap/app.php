5af959af65eccf61b989b857be9fe188225b96d6
Donovan Tengblad <purplefish32@gmail.com>
2014-06-12 16:05:30 +0200

    Branch name

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -12,4 +12,15 @@ $app->post('/', function () use ($app, $config) {
 
     $app->log->info("payload repository name : " . $payload->repository->name);
     $app->log->info("payload ref : " . $payload->ref);
+
+    $prefix = 'refs/heads/';
+
+    $branch = '';
+
+    if (substr($payload->ref, 0, strlen($prefix)) == $prefix) {
+        $branch = substr($payload->ref, strlen($prefix));
+    }
+
+    $app->log->info("payload branch : " . $branch);
+
 });
